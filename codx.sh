d9d9375b7434baf149b406db7f1022b8d5a27e37
Gustavo Brian <gustavo.brian@gmail.com>
2023-02-19 09:11:49 +0100

    Clean and add to global

--- codx.sh
+++ codx.sh
@@ -9,6 +9,8 @@ fi
 sudo apt-update
 sudo apt install -y git
 
+rm -rf codx-cli
 git clone https://github.com/gbrian/codx-cli.git
 
-sudo echo "" >> /etc/profile
+sudo echo "CODX_APPS=${PWD}/codx-cli" >> /etc/environment
+sudo echo "alias codx='${PWD}/codx-cli/codx.sh'" >> /etc/profile

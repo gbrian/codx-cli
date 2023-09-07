331aaa027f9e65c19930aaa5dff78fab5777ede3
Gustavo Brian <gustavo.brian@gmail.com>
2023-09-07 15:52:36 +0200

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,7 +1,5 @@
-# coder/code-server
-curl -sL https://github.com/coder/code-server/releases/download/v4.11.0/code-server_4.11.0_amd64.deb > code-server.deb
+VERSION=${1:-4.16.1}
+curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
+sudo dpkg -i code-server_${VERSION}_amd64.deb
 
-sudo dpkg -i $PWD/code-server.deb
-
-# restart coder
-ps aux | grep "code-server --bind" | for id in `awk {'print$2'}`;do kill &id ;done
+code-server --bind-addr=0.0.0.0:9080 --auth=none

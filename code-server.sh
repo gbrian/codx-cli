34fc314c0c5eadbe73951548d9214f340647e656
Gustavo Brian <gustavo.brian@gmail.com>
2023-09-10 11:54:29 +0200

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 VERSION=${1:-4.16.1}
 curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
 sudo dpkg -i code-server_${VERSION}_amd64.deb
-
-code-server --bind-addr=0.0.0.0:9080 --auth=none
+echo "127.0.0.1:9080 code-server.app" >> /etc/hosts
+code-server --bind-addr=0.0.0.0:9080 --auth=none &

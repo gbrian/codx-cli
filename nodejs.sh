27269f5edcf6d888f39e458f1a4ddf4dd14f25db
Gustavo Brian <gustavo.brian@gmail.com>
2023-09-13 11:15:54 +0200

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,11 +1,13 @@
 [ -s "$NVM_DIR/nvm.sh" ] && exit
-
+VERSION="0.39.5"
 # https://github.com/nvm-sh/nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${VERSION}/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.bashrc
 
 nvm install node
 npm install -g yarn

2398d269a46ed2ac56eda181473761df0b0e93c1
Gustavo Brian <gustavo.brian@gmail.com>
2023-09-20 20:08:08 +0200

    Update aws-docker.sh

--- aws-docker.sh
+++ aws-docker.sh
@@ -3,11 +3,6 @@ sudo yum update
 # Search for Docker package:
 sudo yum install -y docker
 
-# Add group membership for the default ec2-user so you can run all docker commands without using the sudo command:
-sudo usermod -a -G docker ec2-user
-id ec2-user
-# Reload a Linux user's group assignments to docker w/o logout
-newgrp docker
 # Need docker-compose too? Try any one of the following command:
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
@@ -17,3 +12,9 @@ sudo chmod -v +x /usr/local/bin/docker-compose
 sudo systemctl enable docker.service
 # Start the Docker service:
 sudo systemctl start docker.service
+
+# Add group membership for the default ec2-user so you can run all docker commands without using the sudo command:
+sudo usermod -a -G docker ec2-user
+id ec2-user
+# Reload a Linux user's group assignments to docker w/o logout
+newgrp docker

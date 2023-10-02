65638c5bdcc6b39672b451c02663174a232098b9
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-02 19:35:39 +0200

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,4 +1,4 @@
-VERSION=${1:-4.17.0}
+VERSION=${1:-4.17.1}
 curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
 sudo dpkg -i code-server_${VERSION}_amd64.deb
 echo "127.0.0.1:9080 code-server.app" >> /etc/hosts

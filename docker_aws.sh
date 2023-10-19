97e53ae45989d7e66da9d167574e591ceea53fef
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-19 08:02:50 +0200

    Update docker_aws.sh

--- docker_aws.sh
+++ docker_aws.sh
@@ -1,5 +1,3 @@
-DOCKER_COMPOSE_VERSION=1.22.0
-
 # https://gist.github.com/npearce/6f3c7826c7499587f00957fee62f8ee9
 sudo yum install docker -y
 sudo service docker start
@@ -7,5 +5,9 @@ sudo usermod -a -G docker ec2-user
 exec sg docker
 
 sudo chkconfig docker on
-sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_CONPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
+
+# https://stackoverflow.com/a/72156137/1302743
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version

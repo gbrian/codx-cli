ad87b7c2981f252e47799e2743dfff12ac833680
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-25 13:33:36 +0200

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 # Install latest version
-RUN curl -fsSL https://code-server.dev/install.sh | sh
+curl -fsSL https://code-server.dev/install.sh | sh
 # Register host name
 echo "0.0.0.0:9080 code-server" >> /etc/hosts
 # Run in background

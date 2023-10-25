ba0848161885a190c160c041a8fd511f4561db27
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-25 13:32:58 +0200

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -3,4 +3,4 @@ RUN curl -fsSL https://code-server.dev/install.sh | sh
 # Register host name
 echo "0.0.0.0:9080 code-server" >> /etc/hosts
 # Run in background
-sudo -i -u "1000:1000" code-server --bind-addr=0.0.0.0:9080 --auth=none &
+code-server --bind-addr=0.0.0.0:9080 --auth=none &

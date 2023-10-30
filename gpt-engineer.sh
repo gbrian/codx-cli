b566402d798ba2d429d0e1c9bf75e5918359525a
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-30 06:45:45 +0100

    Update gpt-engineer.sh

--- gpt-engineer.sh
+++ gpt-engineer.sh
@@ -2,9 +2,12 @@
 sudo apt update
 sudo apt-get install -y python3-tk make
 
+codx python
+
+rm -rf gpt-engineer
 git clone https://github.com/AntonOsika/gpt-engineer.git
 cd gpt-engineer
-pip install -e .
+python3 -m pip install -e .
 
 GPT_ENGINEER_PATH=$PWD
 echo "alias gpt-engineer='python3 ${GPT_ENGINEER_PATH}/gpt_engineer/cli/main.py'" >> ~/.bashrc

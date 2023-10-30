ad01f8f6d93cbc5fdc073942636a3216568179d1
Gustavo Brian <gustavo.brian@gmail.com>
2023-10-30 07:03:26 +0100

    Update gpt-engineer.sh

--- gpt-engineer.sh
+++ gpt-engineer.sh
@@ -5,8 +5,13 @@ sudo apt-get install -y python3-tk make
 codx python
 
 rm -rf gpt-engineer
-git clone https://github.com/AntonOsika/gpt-engineer.git
+git clone --single-branch --branch codx https://github.com/gbrian/gpt-engineer.git
 cd gpt-engineer
+if [ -z "$OPENAI_API_KEY" ]; then
+    echo "OPENAI_API_KEY not found. Paste the openAI key here or edit $PWD/.env later."
+    read -p "Key:" OPENAI_API_KEY
+fi
+echo "OPENAI_API_KEY=$OPENAI_API_KEY" > .env
 python3 -m pip install -e .
 
 GPT_ENGINEER_PATH=$PWD

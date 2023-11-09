81e6f6704120b5f017e48d29b21d21842cfd50d1
Gustavo Brian <gustavo.brian@gmail.com>
2023-11-09 12:20:32 +0100

    Update gpt-engineer.sh

--- gpt-engineer.sh
+++ gpt-engineer.sh
@@ -12,8 +12,13 @@ if [ -z "$OPENAI_API_KEY" ]; then
     echo "OPENAI_API_KEY not found. Paste the openAI key here or edit $PWD/.env later."
     read -p "Key:" OPENAI_API_KEY
 fi
-echo "OPENAI_API_KEY=$OPENAI_API_KEY" > .env
 python3 -m pip install -e .
 
 GPT_ENGINEER_PATH=$PWD
-echo "alias gpt-engineer='python3 ${GPT_ENGINEER_PATH}/gpt_engineer/cli/main.py'" >> ~/.bashrc
+BASH_RC=/home/$USER/.bashrc
+if ! grep -q $OPENAI_API_KEY "$BASH_RC"; then
+    echo "export OPENAI_API_KEY=$OPENAI_API_KEY" >> $BASH_RC
+fi
+if ! grep -q "gpt-engineer" "$BASH_RC"; then
+    echo "alias gpt-engineer='python3 ${GPT_ENGINEER_PATH}/gpt_engineer/cli/main.py'" >> $BASH_RC
+fi

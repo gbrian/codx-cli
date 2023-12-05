953071f416ce5d5b45d0e41d993af752411b8423
Gustavo Brian <gustavo.brian@gmail.com>
2023-12-05 11:29:48 +0100

    Update jetbrains-toolbox.sh

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -1,6 +1,6 @@
 set -x
 VERSION=2.1.1.18388
 curl -sL "https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-${VERSION}.tar.gz" > jetbrains-toolbox-${VERSION}.tar.gz
-sudo tar -xzf jetbrains-toolbox-1.17.7391.tar.gz -C /opt
+sudo tar -xzf jetbrains-toolbox-${VERSION}.tar.gz -C /opt
 
 echo "Execute jetbrains-toolbox"

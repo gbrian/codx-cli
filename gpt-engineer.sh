ace0b1b7a46c7eca58142f85c50cdedec4404c82
gbrian <gustavo.brian@gmail.com>
2023-12-25 07:37:19 +0000

    fix installer

--- gpt-engineer.sh
+++ gpt-engineer.sh
@@ -1,9 +1,6 @@
 # https://gpt-engineer.readthedocs.io/en/latest/installation.html
-sudo apt update
-sudo apt-get install -y python3-tk make
-
-codx python
 source ~/.bashrc
+BASH_RC=~/.bashrc
 
 rm -rf gpt-engineer
 git clone --single-branch --branch codx https://github.com/gbrian/gpt-engineer.git
@@ -12,13 +9,27 @@ if [ -z "$OPENAI_API_KEY" ]; then
     echo "OPENAI_API_KEY not found. Paste the openAI key here or edit $PWD/.env later."
     read -p "Key:" OPENAI_API_KEY
 fi
-python3 -m pip install -e .
 
-GPT_ENGINEER_PATH=$PWD
-BASH_RC=~/.bashrc
 if ! grep -q $OPENAI_API_KEY "$BASH_RC"; then
     echo "export OPENAI_API_KEY=$OPENAI_API_KEY" >> $BASH_RC
 fi
-if ! grep -q "gpt-engineer" "$BASH_RC"; then
-    echo "alias gpt-engineer='python3 ${GPT_ENGINEER_PATH}/gpt_engineer/cli/main.py'" >> $BASH_RC
+
+if docker --version;then
+  echo "****** INSTALLING DOCKER dgpt-engineer. Docker version"
+  docker build -t gpt-engineer -f docker/Dockerfile .
+
+  sudo cp dgpt-engineer.sh /usr/bin/gpt-engineer
+  sudo chmod +x /usr/bin/gpt-engineer
+else
+  echo "****** DOCKER NOT FOUND. INSTALLING LOCALLY gpt-engineer. Docker version"
+  sudo apt update
+  sudo apt-get install -y python3-tk make
+
+  bash $CODX_APPS/codx.sh python
+  python3 -m pip install -e .
+
+  GPT_ENGINEER_PATH=$PWD
+  if ! grep -q "gpt-engineer" "$BASH_RC"; then
+      echo "alias gpt-engineer='python3 ${GPT_ENGINEER_PATH}/gpt_engineer/cli/main.py'" >> $BASH_RC
+  fi
 fi

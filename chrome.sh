72e1321d13f814b22041a6769accb4f91b8550b2
Gustavo Brian <gustavo.brian@gmail.com>
2024-02-12 09:52:44 +0100

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,7 +1,5 @@
-# https://linuxiac.com/how-to-install-google-chrome-on-debian-11/
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
-sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo cp /usr/bin/google-chrome /usr/bin/chrome
 
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt install -y google-chrome-stable
+rm ./google-chrome-stable_current_amd64.deb

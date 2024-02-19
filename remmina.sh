511edfa5437aa3f40235b9112912bf62874ba158
Gustavo Brian <gustavo.brian@gmail.com>
2024-02-19 10:57:34 +0100

    Update remmina.sh

--- remmina.sh
+++ remmina.sh
@@ -1,3 +1,4 @@
-sudo echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
+echo 'deb http://deb.debian.org/debian bookworm-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
+#echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
 sudo apt update
-sudo apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
+sudo apt install -y -t bookworm-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice

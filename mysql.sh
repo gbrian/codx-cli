25c4109de8293426e36b4bf2e15c0122fae7babc
Gustavo Brian <gustavo.brian@gmail.com>
2024-03-08 12:38:48 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,18 @@
 sudo apt update
-sudo apt install gnupg
+sudo apt install -y gnupg lsb-release
+
+export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select trusty'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
 sudo dpkg -i mysql-apt-config*
 sudo apt update
-sudo apt install -y mysql-server
+sudo apt install -y default-mysql-server
 

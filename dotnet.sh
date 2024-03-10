c9f3b1b6f1d04488c201a517f254c6ac710c8654
Gustavo Brian <gustavo.brian@gmail.com>
2024-03-10 12:34:12 +0100

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -1,4 +1,6 @@
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh --version latest
-sudo ln -s $HOME/.dotnet/dotnet /usr/bin/dotnet
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-update
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-${VERSION:-6.0}

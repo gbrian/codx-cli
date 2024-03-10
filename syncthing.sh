38abef63450022069b7cfc23d2f5ca6254cffeb1
Gustavo Brian <gustavo.brian@gmail.com>
2024-03-10 12:01:44 +0100

    Create syncthing.sh

---
+++ syncthing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#https://apt.syncthing.net/
+# Add the release PGP keys:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+#The candidate channel is updated with release candidate builds, usually every second Tuesday of the month. These predate the corresponding stable builds by about three weeks.
+
+# Add the "candidate" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+#And finally.
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
+syncthing serve &

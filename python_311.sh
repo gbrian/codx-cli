57ccc3a49e3fa316ffcc31c20a005bdbe8867c30
Gustavo Brian <gustavo.brian@gmail.com>
2024-04-02 09:13:00 +0200

    Update python_311.sh

--- python_311.sh
+++ python_311.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Add SID repository to sources.list
-echo "deb http://deb.debian.org/debian/ sid main" | sudo tee -a /etc/apt/sources.list
-
 # Update the package list
 sudo apt update
 
 # Install Python 3.11 from SID repository
-sudo apt install -t sid -y python3.11 python3.11-venv
-sudo apt install -y python3-venv
+sudo apt install -y python3 python3-pip python3-venv
 
 # Verify the installation
-VERSION=$(python3.11 -V)
-
-# Optionally, remove SID repository to avoid potential instability
-sudo sed -i '/deb http:\/\/deb.debian.org\/debian\/ sid main/d' /etc/apt/sources.list
-sudo apt update
+VERSION=$(python3 -V)
 
 echo "Python 3.11 has been successfully installed on your Debian Bullseye system. $VERSION"

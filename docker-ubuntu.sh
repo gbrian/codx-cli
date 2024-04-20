43bc5a1780b70f3a44e754e4305e005e2489959c
Gustavo Brian <gustavo.brian@gmail.com>
2024-04-20 08:38:41 +0200

    Update docker-ubuntu.sh

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -16,3 +16,10 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker --version
 
+if [ ! -x "$(command -v docker-compose)" ]; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+docker-compose --version
+

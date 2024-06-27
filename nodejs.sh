edced035c672fa88395f1731d7c3e85934b86999
Gustavo Brian <gustavo.brian@gmail.com>
2024-06-27 17:07:05 +0200

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,13 +1,12 @@
 [ -s "$NVM_DIR/nvm.sh" ] && exit
-VERSION="0.39.5"
+VERSION="0.39.7"
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${VERSION}/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bashrc
 
 source ~/.bashrc
 
-nvm install node
+nvm install latest
 npm install -g yarn

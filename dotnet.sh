7be8cb04969712c787cedb043083d5b04a1527fd
Gustavo Brian <gustavo.brian@gmail.com>
2024-09-10 15:05:19 +0200

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION="latest"
+DOTNET_VERSION=${DOTNET_VERSION:-latest}
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh --version ${VERSION}
+./dotnet-install.sh --version ${DOTNET_VERSION}
 sudo ln -s $HOME/.dotnet/dotnet /usr/bin/dotnet

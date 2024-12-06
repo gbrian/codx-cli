517b58ba2eb8cec5bc3296d0cbbcfc2d240c2ed8
Gustavo Brian <gustavo.brian@gmail.com>
2024-12-06 08:54:46 +0100

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,5 +1,4 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo cp /usr/bin/google-chrome /usr/bin/chrome
-
-rm ./google-chrome-stable_current_amd64.deb
+sudo rm ./google-chrome-stable_current_amd64.deb

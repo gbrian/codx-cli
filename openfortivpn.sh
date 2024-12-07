a77a4178021546163db3a8a8005e0e3dcf163f7b
Gustavo Brian <gustavo.brian@gmail.com>
2024-12-07 11:12:22 +0100

    Update openfortivpn.sh

--- openfortivpn.sh
+++ openfortivpn.sh
@@ -6,12 +6,29 @@ if [ ! -x "/usr/bin/openfortivpn" ]; then
   deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu xenial multiverse \
   apt-get update
   apt install forticlient
+
+  if [ ! -t "/etc/openfortivpn/openfortivpn.conf" ]; then
+    echo "
+    host = 
+    port = 
+    username = 
+    password = 
+    set-dns = 0
+    pppd-use-peerdns = 0
+    # X509 certificate sha256 sum, trust only this one!
+    trusted-cert = 
+    " > /etc/openfortivpn/openfortivpn.conf
+  fi
 fi
 
 SVPNCOOKIE=${SVPNCOOKIE:-1}
+
 if [ -n "$SVPNCOOKIE" ]; then
-  openfortivpn -c /etc/openfortivpn/my-config
+  openfortivpn -c /etc/openfortivpn/openfortivpn.conf
         -v -v -v \
         --cookie=SVPNCOOKIE=${SVPNCOOKIE}
+else
+  echo "SVPNCOOKIE not found"
+  echo "Usage codx openfortivpn SVPNCOOKIE"
 fi
     

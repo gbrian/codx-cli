6805773d194c686d89a35ee88dc58922e654a797
Gustavo Brian <gustavo.brian@gmail.com>
2025-01-11 09:30:00 +0100

    Update intellij-idea.sh

--- intellij-idea.sh
+++ intellij-idea.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
+idea_source=https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-2.5.2.35332.tar.gz
 
-IDEA_VERSION=2023.1
-IDEA_BUILD=2023.1.2
-idea_local_dir=~/.IdeaIC${IDEA_VERSION}
-
-TARGETARCH=$(uname -m)
-if [ "$TARGETARCH" = "aarch64" ]; then 
-    idea_arch='-aarch64'
-else 
-    idea_arch='' 
-fi
+sudo apt install libfuse2
 
 mkdir -p ~/opt/idea
 
-echo "Preparing IntelliJ IDEA ${IDEA_BUILD} ..."
-echo "Architecture: ${TARGETARCH}"
-idea_source=https://download.jetbrains.com/idea/ideaIC-${IDEA_BUILD}${idea_arch}.tar.gz
+echo "Preparing IntelliJ IDEA ..."
 echo "Downloading ${idea_source} ..."
 curl -fsSL $idea_source -o ~/opt/idea/installer.tgz
 tar --strip-components=1 -xzf ~/opt/idea/installer.tgz -C ~/opt/idea

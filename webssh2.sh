5abfdbd25a2aa22fab4017f2c0b6d7726c25c398
Gustavo Brian <gustavo.brian@gmail.com>
2025-02-10 15:54:10 +0100

    Update webssh2.sh

--- webssh2.sh
+++ webssh2.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+USER_NAME=$1
+USER_PWD=$2
+# create ssh user
+# quietly add a user without password
+adduser --quiet --disabled-password --shell /bin/bash --home /home/${USER_NAME} --gecos "${USER_NAME}" ${USER_NAME}
+# set password
+echo "${USER_NAME}:${USER_PWD}" | chpasswd
+usermod -aG sudo ${USER_NAME}
+
+
 git clone https://github.com/billchurch/webssh2
 cd webssh2
 git checkout current

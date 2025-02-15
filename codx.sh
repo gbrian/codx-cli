b1d0bd49686922e32cb633e975737b006b3a7742
Gustavo Brian <gustavo.brian@gmail.com>
2025-02-15 10:46:06 +0100

    Update codx.sh

--- codx.sh
+++ codx.sh
@@ -31,4 +31,6 @@ sudo apt install -y git
 sudo rm -rf $CODX_APPS
 sudo git clone https://github.com/gbrian/codx-cli.git $CODX_APPS
 sudo chmod +x $CODX_APPS/codx.sh
+# Remove previous installation link
+sudo rm /usr/local/bin/codx || true
 sudo ln -s $CODX_APPS/codx.sh /usr/local/bin/codx

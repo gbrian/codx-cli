a2c5e4f552fd1705f1e3358739bbb5c256f88e1a
Gustavo Brian <gustavo.brian@gmail.com>
2025-03-31 13:08:01 +0200

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
-sudo sh webmin-setup-repo.sh
+sudo sh webmin-setup-repo.sh -f
 sudo apt-get -y install --install-recommends webmin usermin
 sudo /usr/share/webmin/changepass.pl /etc/webmin root 12345
+echo "webprefixnoredir=1" | sudo tee -a /etc/webmin/config
 echo "webadmin availble at http://localhost:10000 
 webmin root password set to 12345
 To change webmin root password run:

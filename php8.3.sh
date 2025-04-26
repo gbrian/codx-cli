f00d17a2081e4be1d54355c9cfc5cc6c265daf38
Gustavo Brian <gustavo.brian@gmail.com>
2025-04-26 13:59:45 +0200

    Create php8.3.sh

---
+++ php8.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install software-properties-common lsb-release apt-transport-https ca-certificates -y
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg 
+sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt update
+sudo apt install -y php8.3

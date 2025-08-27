37386f416425d93b0ad0de29e043e4803c6e31d5
Gustavo Brian <gustavo.brian@gmail.com>
2025-08-27 13:21:46 +0200

    Create putter.sh

---
+++ putter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir ~/puter
+cd ~/puter
+mkdir -p puter/config puter/data
+sudo chown -R 1000:1000 puter
+docker run --rm -p 4100:4100 \
+  -v `pwd`/puter/config:/etc/puter \
+  -v `pwd`/puter/data:/var/puter  \
+  ghcr.io/heyputer/puter

916a8ecacf029cd7ed99c408d49698a666a9813f
Gustavo Brian <gustavo.brian@gmail.com>
2025-10-18 12:18:06 +0200

    Add NuGet credential manager installation script
    Added installation of NuGet credential manager.

--- dotnet.sh
+++ dotnet.sh
@@ -4,3 +4,6 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version ${DOTNET_VERSION}
 sudo ln -s $HOME/.dotnet/dotnet /usr/bin/dotnet
+
+# Nuget credential manager
+curl -sL https://raw.githubusercontent.com/microsoft/artifacts-credprovider/refs/heads/master/helpers/installcredprovider.sh | bash -

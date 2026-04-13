1c1f5dce4d410ab05b9730246b043662fbb2ab3f
Gustavo Brian <gustavo.brian@gmail.com>
2026-04-13 12:55:49 +0200

    Create dbeaver.sh

---
+++ dbeaver.sh
@@ -0,0 +1,3 @@
+sudo wget -q -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce

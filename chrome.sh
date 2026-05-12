8ffebf320de9b3fac125a80a28980db2f792595e
Gustavo Brian <gustavo.brian@gmail.com>
2026-05-12 14:03:02 +0200

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,4 +1,12 @@
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-sudo cp /usr/bin/google-chrome /usr/bin/chrome
-sudo rm ./google-chrome-stable_current_amd64.deb
+#!/bin/bash
+# Download the latest stable Google Chrome package
+wget -O /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install the package and resolve any missing dependencies
+sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
+
+# Create the 'chrome' alias if it doesn't exist
+sudo ln -sf /usr/bin/google-chrome /usr/bin/chrome
+
+# Clean up
+rm /tmp/google-chrome-stable_current_amd64.deb

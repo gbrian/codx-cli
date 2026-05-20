0b28bcff0fc36c8f07be5fe15c771c152086098c
Gustavo Brian <gustavo.brian@gmail.com>
2026-05-20 12:28:32 +0200

    Update Azure Functions installation command
    Remove sudo from npm install command for Azure Functions Core Tools.

--- azure-functions.sh
+++ azure-functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and install the Azure Functions Core Tools
-sudo npm install -g azure-functions-core-tools@4 --unsafe-perm true
+npm install -g azure-functions-core-tools@4 --unsafe-perm true
 
 # Verify installation
 func --version

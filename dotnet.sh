caf9bee1ad1927d60216f42bd9923c66c96c6f90
Gustavo Brian <gustavo.brian@gmail.com>
2026-05-21 18:21:09 +0200

    Refactor dotnet.sh for improved installation process
    Updated the script to use curl for downloading the .NET install script and improved version handling.

--- dotnet.sh
+++ dotnet.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-DOTNET_VERSION=${DOTNET_VERSION:-latest}
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh --version ${DOTNET_VERSION}
-sudo ln -s $HOME/.dotnet/dotnet /usr/bin/dotnet
 
-# Nuget credential manager
+# 1. Determine version: Use $1 if provided, otherwise default to latest
+DOTNET_VERSION=${1:-latest}
+
+echo "Installing .NET version: $DOTNET_VERSION..."
+
+# 2. Download and run the official install script
+curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+chmod +x ./dotnet-install.sh
+
+# Use --channel as it handles both 'latest' and specific versions (8.0, 9.0) better
+./dotnet-install.sh --channel ${DOTNET_VERSION}
+
+# 3. Create symlink (using -f to overwrite if it already exists)
+sudo ln -sf $HOME/.dotnet/dotnet /usr/bin/dotnet
+
+# 4. Nuget credential manager
+echo "Installing NuGet Credential Provider..."
 curl -sL https://raw.githubusercontent.com/microsoft/artifacts-credprovider/refs/heads/master/helpers/installcredprovider.sh | bash -
 
-# Azure cli
+# 5. Azure CLI
+echo "Installing Azure CLI..."
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# 6. Cleanup
+rm dotnet-install.sh
+
+echo "Done! Current dotnet version:"
+dotnet --version

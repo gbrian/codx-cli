d3178db5edaba5e833d00b4af7c21ffa93de1a9d
Gustavo Brian <gustavo.brian@gmail.com>
2026-05-28 15:54:35 +0200

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,4 +1,7 @@
 [ -s "$NVM_DIR/nvm.sh" ] && exit
+
+sudo apt install libatomic1
+
 VERSION="0.40.3"
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${VERSION}/install.sh | bash
